0a1d3dc5246da0feba20e3bc05beab2644dea9bf
raicerk <raicerk@gmail.com>
2019-07-11 17:04:39 -0400

    Se comienza proyecto de sistema de log para nodejs

---
+++ index.js
@@ -0,0 +1,23 @@
+const moment = require('moment');
+
+class nodelog {
+
+    /**
+     * @name register
+     * @description Function for register log of nodejs application in PM2
+     * @param type Type of log, example ['ERROR', 'INFO', 'OTHER']
+     * @param other Field aditional for save in the log
+     */
+    register = (type, other) => {
+        var now = moment().locale("es").format("YYYY-MM-DDTHH:mm:ss:SSSS")
+        let e = new Error();
+        let frame = e.stack.split("\n")[2];
+        let numberline = frame.split(":")[2];
+        let file = frame.split(":")[1]
+        let method = frame.split(" ")[5];
+        console.log(`DateTime[${now}];File['${file}'];NumberLine[${numberline}];Method[${method}];Type[${type}];${other}`);
+    }
+
+}
+
+module.exports = nodelog;
